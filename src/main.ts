6ea9753d93c949c0da239c2751f2e195597d77a2
Tom Hohler <thohler@betomorrow.com>
2024-06-28 11:27:19 +0200

    setup dojo

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,32 @@
+import "./dojo.css";
 import "./style.css";
 
-console.log("Hello World");
+function wireRotationSlider(selector: string, cssVar: string) {
+	document
+		.querySelector<HTMLInputElement>(selector)
+		?.addEventListener("input", (e) =>
+			document.body.style.setProperty(cssVar, (e.target as HTMLInputElement)?.value + "deg")
+		);
+}
+
+wireRotationSlider("#rotate-x", "--camera-rotate-x");
+wireRotationSlider("#rotate-y", "--camera-rotate-y");
+wireRotationSlider("#rotate-z", "--camera-rotate-z");
+
+document
+	.querySelector<HTMLInputElement>("#zoom")
+	?.addEventListener("input", (e) =>
+		document.body.style.setProperty("--camera-zoom", (e.target as HTMLInputElement)?.value)
+	);
+
+document.querySelector<HTMLButtonElement>("#reset")?.addEventListener("click", () => {
+	document.querySelector<HTMLInputElement>("#rotate-x")!.value = "0";
+	document.querySelector<HTMLInputElement>("#rotate-y")!.value = "0";
+	document.querySelector<HTMLInputElement>("#rotate-z")!.value = "0";
+	document.querySelector<HTMLInputElement>("#zoom")!.value = "1";
+
+	document.body.style.setProperty("--camera-rotate-x", "0");
+	document.body.style.setProperty("--camera-rotate-y", "0");
+	document.body.style.setProperty("--camera-rotate-z", "0");
+	document.body.style.setProperty("--camera-zoom", "1");
+});
